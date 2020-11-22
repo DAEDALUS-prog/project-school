b577e63b0c74e5c975da9f8d59bfd77ec09f31aa
PromiSe#### <vicimpa@ya.ru>
2020-11-23 01:15:30 +0300

    Run entrypoint

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "react-dom";
+import { MainComponent } from "./components/Main";
+
+import "./index.sass";
+import { loadQuestions } from "./lib/loadQuestions";
+
+const app = document.getElementById('app')
+
+async function main() {
+  const questions = await loadQuestions()
+
+  console.log(questions)
+
+  for(let {variants} of questions)
+    variants.sort(() => Math.random() > 0.5 ? 1 : -1)
+
+  render(<MainComponent questsions={questions} />, app)
+}
+
+main().catch(console.error)
